acba45bdf36b2770078203682baf484140882282
Pritha Maity <117790222+PrithaMaity@users.noreply.github.com>
2025-03-26 23:21:58 +0530

    Add files via upload

---
+++ 41_login54.php
@@ -0,0 +1,12 @@
+
+<?php
+if($_GET){
+    echo "User name is : ".$_GET['user_name'];
+    echo "<br/>";
+    echo "<br/>";
+    echo "User Password is : ".$_GET['user_password'];
+    
+}
+
+
+?>
---
+++ 42_signup55.php
@@ -0,0 +1,14 @@
+<?php
+
+if($_POST){
+    echo "user name is : ". $_POST['user_name'];
+    echo "<br/>";
+    echo "<br/>";
+    echo "user email is : ". $_POST['user_email'];
+    echo "<br/>";
+    echo "<br/>";
+    echo "user password is : ". $_POST['user_password'];
+
+}
+
+?>
