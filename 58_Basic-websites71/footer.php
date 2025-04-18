09b60b01572d99fb5a30843a2eeb35b610e88908
Pritha Maity <117790222+PrithaMaity@users.noreply.github.com>
2025-04-18 22:59:39 +0530

    Add files via upload

---
+++ 58_Basic-websites71/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+<ul>
+<li><a href="index.php">Home</ahref></li>
+<li><a href="about.php">About</ahref></li>
+<li><a href="contact.php">Contact</ahref></li>
+</ul>
+</footer>
---
+++ 58_Basic-websites71/header.php
@@ -0,0 +1,11 @@
+<div class="header">
+    <h3>
+        LOGO
+    </h3>
+    <ul>
+        <li><a href="index.php">Home</ahref></li>
+        <li><a href="about.php">About</ahref></li>
+        <li><a href="contact.php">Contact</ahref></li>
+
+    </ul>
+</div>
